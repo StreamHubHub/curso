8e846655925cb02ea33c761ef70fc7ee76bf810d
Alex <Alex@git.com>
2022-08-06 14:29:37 -0500

    Se le agrego una descripción al funcionamiento de la aplicación

--- porcentaje.py
+++ porcentaje.py
@@ -1,3 +1,6 @@
 print("""--------------------------
 CALCULADORA DE PORCENTAJE
 --------------------------""")
+
+print("""Para poder calcular el precio final después del impuesto 
+solamente ingresa el precio final deseado y el porcentaje de impuesto que se te cobrara""")
