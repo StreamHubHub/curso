e738ef09c6e58ce0124d159dff2f27a4c8af1916
Alex <Alex@git.com>
2022-08-06 14:48:02 -0500

    Se la agrego variable al precio final deseado y al impuesto cobrado

--- porcentaje.py
+++ porcentaje.py
@@ -5,6 +5,6 @@ CALCULADORA DE PORCENTAJE
 print("""Para poder calcular el precio final después del impuesto 
 solamente ingresa el precio final deseado y el porcentaje de impuesto que se te cobrara""")
 
-input("""
+precio_final = input("""
 Inserte el precio final deseado: """)
-input ("Ingrese el impuesto cobrado: ")
+impuesto_cobrado = input ("Ingrese el impuesto cobrado: ")
