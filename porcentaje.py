8c88c56e9e83f5337491c52b3d05bb573243ca67
Alex <cuentagrupofamiliar@gmail.com>
2022-08-09 20:46:33 -0500

    Corrección de la impresión del resultado

--- porcentaje.py
+++ porcentaje.py
@@ -5,8 +5,8 @@ CALCULADORA DE PORCENTAJE
 print("""Para poder calcular el precio final después del impuesto 
 solamente ingresa el precio final deseado y el porcentaje de impuesto que se te cobrara""")
 
-precio_final = int(input("""
-Inserte el precio float deseado: """))
+precio_final = float(input("""
+Inserte el precio deseado: """))
 impuesto_cobrado = float(input ("Ingrese el impuesto cobrado: "))
 
 contador = float(0)
@@ -15,4 +15,4 @@ while contador < precio_final:
     contador = incremento - (incremento*impuesto_cobrado)
     incremento = incremento + 1
 
-print("El precio final debe ser: ",contador)
+print("El precio final debe ser: ",incremento)
